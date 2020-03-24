e1c7c647bb7e1757c5c6809260b3fbcc814a6792
nwsctyhh <62599231+nwsctyhh@users.noreply.github.com>
2020-03-24 20:32:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,73 +1,144 @@
-var correct = new Audio('se/correct.mp3');
-var incorrect = new Audio('se/incorrect.mp3');
-var decision = new Audio('se/decision.mp3');
-var next = new Audio('se/question.mp3');
-let quiz=0
-let answer=0
-let o=[0,0,0,0,0,0,0,0,0]
-let x=[0,0,0,0,0,0,0,0,0]
-document.onkeydown = function (e){
-    if(quiz==0){return}
-	if(!e){e = window.event}
-    let key_code = e.keyCode;
-    if (key_code=="49"){
-        answer=1
-    }else if(key_code==50){
-        answer=2
-    }else if(key_code==56){
-        answer=3
-    }else if(key_code==57){
-        answer=4
-    }else if(key_code==90){
-        answer=5
-    }else if(key_code==88){
-        answer=6
-    }else if(key_code==78){
-        answer=7
-    }else if(key_code==77){
-        answer=8
-    }else{
-        return;
-    }
-    quiz=0
-    document.getElementById("p"+answer).style.backgroundColor="#ff0000"
-    decision.play();
-};
-function start(){
-    if(answer>0){
-        return;
-    }
-    if(document.getElementById("startreset").innerHTML=="start"){
-        document.getElementById("startreset").style.display="none"
-    }
-    for(let i=1;i<9;i++){
-        document.getElementById("ox"+i).innerHTML=o[i]+"o"+x[i]+"x"
-        document.getElementById("p"+i).style.backgroundColor="#ffffff"
-    }
-    quiz=1
-    next.play()
-}
-function question(ox){
-    if(answer==0){
-        return;
-    }
-    if(ox=='o'){
-        correct.play()
-        alert(answer+"P:正解")
-        o[answer]++
-    }else if(ox=='x'){
-        incorrect.play()
-        alert(answer+"P:不正解")
-        x[answer]++
-    }else{
-        alert(answer+"P:スルー")
-    }
-    for(let i=1;i<9;i++){
-        document.getElementById("ox"+i).innerHTML=o[i]+"o"+x[i]+"x"
-        document.getElementById("p"+i).style.backgroundColor="#ffffff"
-    }
-    answer=0;
-    alert("次の問題です")
-    quiz=1
-    next.play()
-}
+(function() {
+	const SEDict = {
+		correct:	new Audio('se/correct.mp3'),
+		incorrect:	new Audio('se/incorrect.mp3'),
+		decision:	new Audio('se/decision.mp3'),
+		next:		new Audio('se/question.mp3'),
+	};
+
+	for (const [_, se] of Object.entries(SEDict))
+		se.load();
+
+
+	const NOT_STARTED		= -1;
+	const CAN_ANSWER		= 0;
+	const FIRST_UID			= 1;
+	const ANSWERING_UID_1	= 1;
+	const ANSWERING_UID_2	= 2;
+	const ANSWERING_UID_3	= 3;
+	const ANSWERING_UID_4	= 4;
+	const ANSWERING_UID_5	= 5;
+	const ANSWERING_UID_6	= 6;
+	const ANSWERING_UID_7	= 7;
+	const ANSWERING_UID_8	= 8;
+	const LAST_UID			= 8;
+
+	let appState = NOT_STARTED;
+
+	const score = [];
+	for ( let uid = FIRST_UID; uid <= LAST_UID; ++uid )
+		score[uid] = { correct: 0, incorrect: 0 };
+
+	const keyCode2UID = (keyCode) => {
+		switch (keyCode) {
+			case 49: return ANSWERING_UID_1;
+			case 50: return ANSWERING_UID_2;
+			case 56: return ANSWERING_UID_3;
+			case 57: return ANSWERING_UID_4;
+			case 90: return ANSWERING_UID_5;
+			case 88: return ANSWERING_UID_6;
+			case 78: return ANSWERING_UID_7;
+			case 77: return ANSWERING_UID_8;
+			default: return null;
+		}
+	};
+
+	const updateUI = () => {
+		for (let uid = FIRST_UID; uid <= LAST_UID; ++uid) {
+			document.getElementById(
+				`p${uid}`
+			).style.backgroundColor = appState == uid ? '#f66' : 'unset';
+
+			document.getElementById(
+				`ox${uid}`
+			).textContent = `${score[uid].correct}o${score[uid].incorrect}x`;
+		}
+	};
+
+	document.addEventListener('keydown', (e) => {
+		if (appState != CAN_ANSWER) return;
+
+		const answererUid = keyCode2UID(e.keyCode);
+		if (!answererUid) return;
+		appState = answererUid;
+
+		updateUI();
+	});
+
+	// start button (once)
+	document.getElementById('startreset').addEventListener('click', (e) => {
+		e.target.style.display = 'none';
+		
+		appState = CAN_ANSWER;
+		updateUI();
+		SEDict.next.play();
+	});
+
+	const O_BUTTON = 0;
+	const X_BUTTON = 1;
+	const PASS_BUTTON = 3;
+	const buttons = document.getElementsByTagName('a');
+
+	const nextQuiz = () => {
+		appState = CAN_ANSWER;
+		updateUI();
+
+		alert("次の問題です");
+		SEDict.next.play();
+	};
+
+	buttons[O_BUTTON].addEventListener('click', () => {
+		if (appState < FIRST_UID) return;
+
+		SEDict.correct.play();
+		alert (`${appState}P 正解`);
+		++ score[appState].correct;
+
+		nextQuiz();
+	});
+
+	buttons[X_BUTTON].addEventListener('click', () => {
+		if (appState < FIRST_UID) return;
+
+		SEDict.incorrect.play();
+		alert (`${appState}P 不正解`);
+		++ score[appState].incorrect;
+
+		nextQuiz();
+	});
+
+	buttons[PASS_BUTTON].addEventListener('click', () => {
+		if (appState < FIRST_UID) return;
+		alert (`${appState}P パス`);
+
+		nextQuiz();
+	});
+
+
+	{
+		const volume = document.createElement('input');
+		volume.max = 1;
+		volume.min = 0;
+		volume.step = .01;
+		volume.value = localStorage.getItem('quiz-self_volume') ?? .3;
+		volume.type = 'range';
+		volume.style.width = '100%';
+
+		document.body.appendChild(volume);
+
+		const applyVolume = (() => {
+			for (const [_, se] of Object.entries(SEDict))
+				se.volume = volume.value;
+
+			localStorage.setItem('quiz-self_volume', volume.value);
+		});
+
+		volume.addEventListener('change', applyVolume);
+		applyVolume();
+	}
+
+})();
+
+function start() {}
+function question() {}
+
